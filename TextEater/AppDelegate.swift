e0dbfd723ee0894be04ce9574323133915950e1f
Keaton Burleson <keaton.burleson@me.com>
2021-05-05 16:22:43 -0500

    Working example

--- TextEater/AppDelegate.swift
+++ TextEater/AppDelegate.swift
@@ -10,17 +10,46 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    @IBOutlet weak var menu: NSMenu?
+
+    var statusItem: NSStatusItem?
+    var selectionHandler: SelectionHandler?
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return false
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        self.selectionHandler = SelectionHandler(delegate: self)
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.button?.title = "TextEater"
+
+        if let menu = menu {
+            statusItem?.menu = menu
+        }
+    }
     
 
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    
+    @IBAction func captureToggle(sender: Any) {
+        self.selectionHandler?.startListening()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @IBAction func quitApp(sender: Any) {
+        NSApp.terminate(self)
     }
-
-
 }
 
+extension AppDelegate: SelectionHandlerDelegate {
+    func resultsAvailable(text: String) {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(text, forType: .string)
+        
+        print("Text Result: \(text)")
+        self.selectionHandler?.stopListening()
+    }
+}
