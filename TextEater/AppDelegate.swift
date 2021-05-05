af79e73ab8bdd3f7a08cbf5dbab7ab8acb5447aa
Keaton Burleson <keaton.burleson@me.com>
2021-05-05 13:03:54 -0500

    Initial Commit

---
+++ TextEater/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextEater
+//
+//  Created by Keaton Burleson on 5/5/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TextEater/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TextEater
+//
+//  Created by Keaton Burleson on 5/5/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
